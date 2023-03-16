05709e114c20d5a9ade9f6e34ae85ad7a7c51769
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2023-03-17 01:07:02 +0600

    auth/signup 3

--- components/utility/inputs/LoginInput.js
+++ components/utility/inputs/LoginInput.js
@@ -2,20 +2,35 @@ import { BiUser } from "react-icons/bi";
 import { IoKeyOutline } from "react-icons/io5";
 import styles from "./styles.module.scss";
 import { SiMinutemailer } from "react-icons/si";
+import { MdEmail } from "react-icons/md";
+import { useField } from "formik";
 
-const LoginInput = ({ icon, placeholder }) => {
+const LoginInput = ({ icon, placeholder, ...props }) => {
+  const [field, meta] = useField(props);
+  let iconType =
+    icon === "user" ? (
+      <BiUser />
+    ) : icon === "email" ? (
+      <MdEmail />
+    ) : icon === "password" ? (
+      <IoKeyOutline />
+    ) : (
+      ""
+    );
   return (
-    <div className={styles.input}>
-      {icon === "user" ? (
-        <BiUser />
-      ) : icon === "email" ? (
-        <siMinutemailer />
-      ) : icon === "password" ? (
-        <IoKeyOutline />
-      ) : (
-        ""
-      )}
-      <input type="text" placeholder={placeholder} />
+    <div
+      className={`${styles.input} ${
+        meta.touched && meta.error ? styles.error : ""
+      } `}
+    >
+      {iconType}
+      <input
+        type={field.type}
+        name={field.name}
+        placeholder={placeholder}
+        {...field}
+        {...props}
+      />
     </div>
   );
 };
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,13 +1,34 @@
 import Footer from "components/footer";
 import Header from "components/header";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/signin.module.scss";
 import { BiLeftArrowAlt } from "react-icons/bi";
 import Link from "next/link";
 import { Form, Formik } from "formik";
 import LoginInput from "components/utility/inputs/LoginInput";
+import * as Yup from "yup";
 
-const signin = () => {
+const initialValues = {
+  login_email: "",
+  login_password: "",
+};
+
+const SignIn = () => {
+  const [user, setUser] = useState(initialValues);
+  const { login_email, login_password } = user;
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required("Email is required")
+      .email("Please enter a valid email"),
+    login_password: Yup.string().required("Password is required"),
+  });
+
+  console.log(user);
+
   return (
     <>
       <Header />
@@ -27,10 +48,30 @@ const signin = () => {
             <p>
               Get access to one of the best E-shopping services in the world.
             </p>
-            <Formik>
+            <Formik
+              enableReinitialize
+              initialValues={{
+                login_email,
+                login_password,
+              }}
+              validationSchema={loginValidation}
+            >
               {(form) => (
                 <Form>
-                  <LoginInput icon="user" placeholder="Your Email" />
+                  <LoginInput
+                    type="text"
+                    name="login_email"
+                    onChange={handleChange}
+                    icon="email"
+                    placeholder="Enter Your Email"
+                  />
+                  <LoginInput
+                    type="password"
+                    name="login_password"
+                    onChange={handleChange}
+                    icon="password"
+                    placeholder="Enter Your Password"
+                  />
                 </Form>
               )}
             </Formik>
@@ -42,4 +83,4 @@ const signin = () => {
   );
 };
 
-export default signin;
+export default SignIn;
